328829532e794ae7f8a9d35b2bca7437f2294585
dpolsot1307 <dpolsot1307@g.educaand.es>
2025-10-16 10:21:02 +0200

    Add files via upload

---
+++ ejercicio19.py
@@ -0,0 +1,7 @@
+i = 0
+while i < 100 :
+    print("Hola")
+
+
+for i in range(100):
+    print("Hola")
---
+++ ejercicio20.py
@@ -0,0 +1,6 @@
+num = int(input("Introduzca hasta donde contar:"))
+i = 1
+while i < num:
+    print(i,end=',')
+    i = i + 1
+print(i)
